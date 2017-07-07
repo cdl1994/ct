5d783b1ed32ce21fc1baf81b052787436464f7fb
Mingjie Zhu <Cccmm002@hotmail.com>
2017-07-07 12:03:18 -0700

    package and JSON

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import HomePage from './homepage.js'
 
 
 ReactDOM.render(<HomePage/>, document.getElementById('root'));
 
-
-registerServiceWorker();
