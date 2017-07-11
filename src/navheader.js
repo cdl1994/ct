f2d4c136517cdbf6b4171ba2bceda978d2c25eaa
Chen Dengle <648728073@qq.com>
2017-07-11 13:36:37 -0700

    Finish navbar

--- src/navheader.js
+++ src/navheader.js
@@ -8,17 +8,18 @@ class NavHeader extends React.Component{
 		super();	
 		this.state = {
 			buttonOnSelect : -1,
+			navSubIndex : 2,
 		}	
 	}
 
-	changeButtonBG(value){
-		if (this.state.buttonOnSelect<0) {
-			this.setState({buttonOnSelect : value});
-		}
-		else{
-			this.setState({buttonOnSelect : -1});
-		}	
-		console.log(this.state.buttonOnSelect);
+	mouseOverChange(value){
+		console.log("over");
+		this.setState({buttonOnSelect : value});
+	}
+
+	mouseOutChange(value){
+		console.log("out");
+		this.setState({buttonOnSelect : -1});
 	}
 	
 	render(){
@@ -30,33 +31,50 @@ class NavHeader extends React.Component{
 		}
 		var navTitleStyle = {
 			color : "white",
-			fontSize : "20px",
+			fontSize : "30px",
 			fontFamily : "Tradegothic-stdbold",
 			margin : "0",
 		}
 		var itemStyle = {
 			backgroundColor : "transparent",
+			marginLeft : "0px",
+			display : "inline-block",
+			float : "none",
 		}
 		var itemHighlightStyle = {
-			backgroundColor : "#ccc"
+			backgroundColor : "#ccc",
+			marginLeft : "0px",
+			display : "inline-block",
+			float : "none",
+		}
+		var subListStyle = {
+			position : "absolute",
+			position: "absolute",
+			zIndex : "20",
+			background : "rgba(0,0,0,.6)",
+			padding : "10px",
+			width : "330px",
+			marginLeft : "-50px",
+			marginTop: "10px",
 		}
 		var displayList = this.props.list.map((item,i)=>{
 			if (i==this.state.buttonOnSelect){
 				// hard code
-				if (i==2){
-				var subList = this.props.subList.map((item)=><li><span style={navTitleStyle}>{item}</span></li>);
-				return (<NavItem onMouseOver={this.changeButtonBG.bind(this,i)} onMouseOut={this.changeButtonBG.bind(this,i)} style={itemHighlightStyle}>
-					<p style={navTitleStyle}>{item}</p>
-						<ul>
+				if (i==this.state.navSubIndex){
+				var subList = this.props.subList.map((item)=><div style={navTitleStyle}>{item}</div>);
+				return (
+					<NavItem onMouseEnter={this.mouseOverChange.bind(this,i)} style={itemHighlightStyle}>
+					<p style={navTitleStyle}>{item}</p>					
+					<div onMouseLeave={this.mouseOutChange.bind(this,i)} style={subListStyle}>
 						{subList}
-						</ul>
-					</NavItem>)
+					</div>
+					</NavItem>);
 				}
-				return (<NavItem onMouseOver={this.changeButtonBG.bind(this,i)} onMouseOut={this.changeButtonBG.bind(this,i)} style={itemHighlightStyle}>
+				return (<NavItem onMouseOver={this.mouseOverChange.bind(this,i)} onMouseOut={this.mouseOutChange.bind(this,i)} style={itemHighlightStyle}>
 					<p style={navTitleStyle}>{item}</p></NavItem>);
 			}
 			else{
-				return (<NavItem onMouseOver={this.changeButtonBG.bind(this,i)} onMouseOut={this.changeButtonBG.bind(this,i)} style={itemStyle}>
+				return (<NavItem onMouseOver={this.mouseOverChange.bind(this,i)} onMouseOut={this.mouseOutChange.bind(this,i)} style={itemStyle}>
 					<p style={navTitleStyle}>{item}</p></NavItem>);
 			}
 			});
@@ -64,7 +82,7 @@ class NavHeader extends React.Component{
 		return (
 			<Col md={12} style={colStyle}>
 			<Grid style={colStyle}>
-			<Nav {...settings}>
+			<Nav {...settings} className="header-navigation">
 				{displayList}
 			</Nav>
 			</Grid>
