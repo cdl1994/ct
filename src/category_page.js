90790d04dea47cc8f35248901b5309723502c8b3
Mingjie Zhu <Cccmm002@hotmail.com>
2017-07-20 15:59:37 -0700

    last part

--- src/category_page.js
+++ src/category_page.js
@@ -6,11 +6,23 @@ import Slider from 'react-slick';
 import axios from 'axios';
 import ReadMore from './read_more.js';
 import EnterEmail from './enter_email.js';
+import Columns from 'react-columns';
+import CutBG from './cut_bg.js';
+import Footer from './footer.js';
 
 function textProcess(text) {
     var res = text.replace(/<\/p><p>/g, " ");
     res = res.replace(/<\/p>/g, "");
     res = res.replace(/<p>/g, "");
+    return res;
+}
+
+function phoneFormatter(phone) {
+    var len = phone.length;
+    if (len !== 10) return phone;
+    var res = "(" + phone.slice(0, 3);
+    res += ") " + phone.slice(3, 6);
+    res += "-" + phone.slice(6, 10);
     return res;
 }
 
@@ -44,6 +56,16 @@ class CategoryPage extends React.Component {
         .catch(function (error) {
             console.log(error);
         });
+        axios({
+            method:"get",
+            url:"http://" + address + "/index.php?route=common/cp_footer/api" + suffix,
+        })
+        .then((response)=> {
+            this.setState({footer: response.data});
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
     }
 
     componentWillMount() {
@@ -51,23 +73,44 @@ class CategoryPage extends React.Component {
     }
 
     render() {
+        var footer = this.state.footer == null ? null : <Footer data={this.state.footer} />;
         if (this.state.data != null) {
             var pageStyle = {
                 backgroundColor : this.state.data.theme.backgroundColor,
                 color : this.state.data.theme.lightTextColor,
                 fontStyle : this.state.data.theme.fontStyle,
             };
+            var col_queries = [{
+                columns: 1,
+                query: 'min-width: 1px'
+            },
+            {
+                columns: 3,
+                query: 'min-width: 970px'
+            }];
+            const related = this.state.data.category_page_info.blogs.map((blog) => 
+                <a href={blog.link} key={blog.link} className="related-article-title">{blog.title}</a>
+            );
+            const also = this.state.data.category_page_info.also_loves.map((love) => 
+                <div className="h3 related-ct" key={love.link}>
+                    <a href={love.link}>
+                        <img src={love.template} />
+                        <br />
+                        <p className="related-category-title">{love.name}</p>
+                    </a>
+                </div>
+            );
             return (
-                <DocumentTitle title={this.state.data.category_page_info.category_info.title}>
+                <DocumentTitle title={this.state.data.category_page_info.category_titles.title}>
                 <div style={pageStyle}>
                 <Grid className="ctcategory"><Row>
                     <Col md={6} xs={12}>
-                        <div className="ct-title"><h2 className="centered-line">{this.state.data.category_page_info.category_info.title}</h2></div>
+                        <div className="ct-title"><h2 className="centered-line">{this.state.data.category_page_info.category_titles.title}</h2></div>
                     </Col>
                 </Row></Grid>
-                <WhiteLine image={"/" + this.state.data.theme.sectionDivider}/>
+                <WhiteLine image={this.state.data.theme.sectionDivider}/>
                 <TestimonialCarousal list={this.state.data.category_page_info.testimonials} />
-                <WhiteLine image={"/" + this.state.data.theme.sectionDivider}/>
+                <WhiteLine image={this.state.data.theme.sectionDivider}/>
                 <Grid><Row>
                     <div className="ct-intro-left more" id="read_more_box">
                         <ReadMore lines={4}>
@@ -78,11 +121,38 @@ class CategoryPage extends React.Component {
                         <img id="ct-intro-img" src={this.state.data.category_page_info.category_info.image} />
                     </div>
                 </Row></Grid>
-                <WhiteLine image={"/" + this.state.data.theme.sectionDivider}/>
+                <WhiteLine image={this.state.data.theme.sectionDivider}/>
                 <Customize title={this.state.data.category_page_info.category_info.template_section_title} data={this.state.data.category_page_info.templates} />
-                <WhiteLine image={"/" + this.state.data.theme.sectionDivider}/>
+                <WhiteLine image={this.state.data.theme.sectionDivider}/>
                 <EnterEmail />
-                <WhiteLine image={"/" + this.state.data.theme.sectionDivider}/>
+                <WhiteLine image={this.state.data.theme.sectionDivider}/>
+                <Grid className="related">
+                    <Row><h3 className="centered-line">Related Articles</h3></Row>
+                    <Row><Columns queries={col_queries} gap="12px">
+                        {related}
+                    </Columns></Row>
+                </Grid>
+                <CutBG imageURL={this.state.data.theme.backgroundImage} />
+                <Grid>
+                    <Row className="related related_categories centered-line"><h3>{this.state.data.category_page_info.category_titles.relatedTitle}</h3></Row>
+                    <Row><Columns queries={col_queries} gap="15px">
+                        {also}
+                    </Columns></Row>
+                </Grid>
+                <CutBG imageURL={this.state.data.theme.backgroundImage} />
+                <Grid><Row>
+                    <div className="column-left margin-10 category-ad-btm-l">
+                        <img id="category-ad-btm-img" src={this.state.data.category_page_info.footer.image} />
+                    </div>
+                    <div className="column-right margin-10 category-ad-btm-r">
+                        <h3>{this.state.data.category_page_info.footer.title}</h3>
+                        <p>{this.state.data.category_page_info.footer.text}</p>
+                        <div className="h3 ct-tel">
+                            <a href={"tel:" + this.state.data.category_page_info.footer.phone}>{phoneFormatter(this.state.data.category_page_info.footer.phone)}</a>
+                        </div>
+                    </div>
+                </Row></Grid>
+                {footer}
                 </div>
                 </DocumentTitle>
             );
